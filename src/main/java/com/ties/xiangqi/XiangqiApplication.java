d523410ecdbb1a74d017f5c77b4f2a5f3c187b36
duybo <boddhe187150@fpt.edu.vn>
2025-09-13 23:55:22 +0700

    init commit

---
+++ src/main/java/com/ties/xiangqi/XiangqiApplication.java
@@ -0,0 +1,13 @@
+package com.ties.xiangqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiangqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiangqiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ties/xiangqi/XiangqiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ties.xiangqi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiangqiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
